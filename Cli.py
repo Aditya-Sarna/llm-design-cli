d1bc0771c1d9ad858e40d39585aa0a9b36033463
Aditya-Sarna <107273722+Aditya-Sarna@users.noreply.github.com>
2026-01-14 11:42:27 +0530

    Refactor Cli.py to add goodbye command

--- Cli.py
+++ Cli.py
@@ -1,7 +1,12 @@
-import Typer
-app=Typer.typer()
-@app.command 
-def hello(name:string):
-  print(f"welcome! {name})
-if __name__="__main__"
-        app()
+import typer
+app=typer.Typer()
+@app.command()
+def hello(name:str):
+  print(f"welcome! {name}")
+@app.command()
+def goodbye(name_x:str):
+  print( f"goodbye{name_x}")
+if __name__=="__main__":
+  app()
+  
+
